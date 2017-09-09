44bae529e9cbeb814a493348e7ebb848fc6f1384
yangmillstheory <v.alvarez312@gmail.com>
2017-09-08 20:36:56 -0700

    fix script

--- scripts/test-email-receiver.sh
+++ scripts/test-email-receiver.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 export AWS_PROFILE=yangmillstheory
+export allowed_senders=v.alvarez312@gmail.com,hchaides@gmail.com
 export email_prefix=budget
 export email_bucket=yangmillstheory-email
 export csv_bucket=yangmillstheory-budget
