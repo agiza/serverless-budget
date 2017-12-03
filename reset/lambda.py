9cb6bd9e6e29760bd11305a46aa9dbb23f4f91a2
Victor Alvarez <v.alvarez312@gmail.com>
2017-12-03 09:03:21 -0800

    lint

--- reset/lambda.py
+++ reset/lambda.py
@@ -5,7 +5,7 @@ import logging
 import sys
 
 
-LOCAL_CSV_PATH   = '/tmp/{}'.format(os.getenv('csv_key'))
+LOCAL_CSV_PATH = '/tmp/{}'.format(os.getenv('csv_key'))
 MAX_PERIOD_SPEND = float(os.getenv('max_period_spend'))
 
 
@@ -14,7 +14,8 @@ def get_logger():
     for handler in logger.handlers:
         logger.removeHandler(handler)
     handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(logging.Formatter('[%(levelname)s] %(module)s:%(lineno)s %(message)s'))
+    handler.setFormatter(
+        logging.Formatter('[%(levelname)s] %(module)s:%(lineno)s %(message)s'))
     logger.addHandler(handler)
     logger.setLevel(logging.INFO)
     return logger
@@ -22,12 +23,13 @@ def get_logger():
 
 logger = get_logger()
 sns = boto3.client('sns')
-s3  = boto3.client('s3')
+s3 = boto3.client('s3')
 
 
 def _download_csv():
     """Downloads the budget csv from S3."""
-    s3.download_file(os.getenv('csv_bucket'), os.getenv('csv_key'), LOCAL_CSV_PATH)
+    s3.download_file(
+        os.getenv('csv_bucket'), os.getenv('csv_key'), LOCAL_CSV_PATH)
 
 
 def _reset_csv():
@@ -47,18 +49,22 @@ def _reset_csv():
 
 
 def _notify_period_spend():
-    """Sums all the prices in the price column of the csv and sends a notification with the results."""
+    """Sums all the prices in the price column of the csv
+    and sends a notification with the results.
+    """
     with open(LOCAL_CSV_PATH, newline='') as f:
         prices = [float(row['price']) for row in csv.DictReader(f)]
     period_spend = sum(prices)
     logger.info('Sending summary notification.')
     message_lines = ['Budget period ending!']
-    message_lines.append('Total spent for this period is ${:.2f}'.format(period_spend))
+    message_lines.append(
+        'Total spent for this period is ${:.2f}'.format(period_spend))
     delta = MAX_PERIOD_SPEND - period_spend
     if delta < 0:
         message_lines.append('You went ${:.2f} over budget!'.format(-delta))
     else:
-        message_lines.append('Made it with ${:.2f} savings. Way to go!'.format(delta))
+        message_lines.append(
+            'Made it with ${:.2f} savings. Way to go!'.format(delta))
     sns.publish(
         TopicArn=os.getenv('sns_topic_arn'),
         Message=os.linesep.join(message_lines)
