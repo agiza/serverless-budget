fde5671d661dc84816fa93b186285579ad8f6a7e
Victor Alvarez <v.alvarez312@gmail.com>
2017-12-08 22:22:06 -0800

    add placeholder for required env vars

--- scripts/test-email-receiver.sh
+++ scripts/test-email-receiver.sh
@@ -5,6 +5,7 @@ export email_prefix=budget
 export email_bucket=yangmillstheory-email
 export csv_bucket=yangmillstheory-budget
 export csv_key=budget.csv
+export allowed_senders=
 export sns_topic_arn=arn:aws:sns:us-west-2:079529114411:budget-update-ok
 export dry_run=1
 export max_period_spend=250
