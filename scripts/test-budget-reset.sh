f78ce9884a92bfe3784dfdb29fffa9646972c5a6
Victor Alvarez <v.alvarez312@gmail.com>
2017-12-09 12:14:01 -0800

    do dry run by default

--- scripts/test-budget-reset.sh
+++ scripts/test-budget-reset.sh
@@ -5,7 +5,7 @@ export csv_key=budget.csv
 export csv_template_key=budget.csv.tmpl
 export sns_topic_arn=arn:aws:sns:us-west-2:079529114411:budget-reset
 export reset_recipients=
-# export dry_run=1
+export dry_run=1
 export max_period_spend=250
 python3 reset/lambda.py
 
